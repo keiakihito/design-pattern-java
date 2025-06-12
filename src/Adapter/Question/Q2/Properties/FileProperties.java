9e1da9745b8706be9747eb47e7e88fa1d9491e6b
keiakihito <kkatsumi.git@gmail.com>
2025-06-12 08:45:18 -0700

    Worked Adapater pattern Q2

--- src/Adapter/Question/Q2/Properties/FileProperties.java
+++ src/Adapter/Question/Q2/Properties/FileProperties.java
@@ -1,1 +1,41 @@
 // FilePropertiesクラスを実装してください。
+
+package Adapter.Question.Q2.Properties;
+
+import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
+
+public class FileProperties implements FileIO {
+
+    private Properties properties;
+
+    public FileProperties() {
+        properties = new Properties();
+    }
+
+    @Override
+    public void readFromFile(String filename) throws IOException {
+        try (Reader reader = new FileReader(filename)) {
+            properties.load(reader);
+        }
+    }
+
+    @Override
+    public void writeToFile(String filename) throws IOException {
+        try (Writer writer = new FileWriter(filename)) {
+            properties.store(writer, "written by FileProperties");
+        }
+    }
+
+    @Override
+    public void setValue(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    @Override
+    public String getValue(String key) {
+        return properties.getProperty(key);
+    }
+}
+
--- src/Adapter/Question/Q2/Properties/Main.java
+++ src/Adapter/Question/Q2/Properties/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         FileIO f = new FileProperties();
         try {
-            f.readFromFile("file.txt");
+            f.readFromFile("src/Adapter/Question/Q2/Properties/file.txt");
             f.setValue("width", "1024");
             f.setValue("height", "512");
             f.setValue("depth", "32");
-            f.writeToFile("newfile.txt");
+            f.writeToFile("src/Adapter/Question/Q2/Properties/newfile.txt");
             System.out.println("newfile.txt is created.");
         } catch (IOException e) {
             e.printStackTrace();
