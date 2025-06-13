38e44a64190b0c78db2c0d339e75c78fb996d3d0
keiakihito <kkatsumi.git@gmail.com>
2025-06-13 08:00:50 -0700

    Study TempelateMethod with Sample

---
+++ src/TemplateMethod/Sample/Main.java
@@ -0,0 +1,15 @@
+package TemplateMethod.Sample;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Liskov Substitution Principle:
+        // We can replace the AbstractDisplay with the CharDisplay or the StringDisplay
+        // and the program will still work
+        // 全てのサブクラスはスーパークラスの型として使っても問題なく動作するべき。
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("Hello, world.");
+        d1.display();
+        d2.display();
+    }
+}
